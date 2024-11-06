b53745c7121f0dc702d6556c62be60711e72012e
Tetka95 <martina.bobek95@gmail.com>
2024-11-06 21:18:58 +0100

    dovršen aside, popravljen search bar, uređena tablica(djelomično)

--- script.js
+++ script.js
@@ -12,15 +12,34 @@ const select = document.getElementById('country');
         })
         .catch(error => console.error('Error fetching country data:', error));
 
-document.getElementById('toggle-shipping').addEventListener('click', function() {
-    const shippingDetails = document.getElementById('shipping-details');
-    if (shippingDetails.style.display === 'none' || !shippingDetails.style.display) {
-        shippingDetails.style.display = 'block';
+document.getElementById("toggle-shipping").addEventListener("click", function() {
+    const sidebar = document.querySelector(".sidebar");
+    const shippingDetails = document.getElementById("shipping-details");
+        
+        if (shippingDetails.style.display === "block") {
+            shippingDetails.style.display = "none";
+            sidebar.classList.remove("expanded");
+        } else {
+            shippingDetails.style.display = "block";
+            sidebar.classList.add("expanded");
+        }
+    });
+
+const toggleButton = document.getElementById('toggle-shipping');
+const shippingText = document.querySelector('.shipping p');
+const plusIcon = document.querySelector('#toggle-shipping img');
+
+toggleButton.addEventListener('click', () => {
+    if (plusIcon.src.includes('plus-icon.svg')) {
+        shippingText.style.color = '#008421';
+        plusIcon.src = './assets/x-icon.svg';
+        plusIcon.alt = 'x';
     } else {
-        shippingDetails.style.display = 'none';
+        shippingText.style.color = '';
+        plusIcon.src = './assets/plus-icon.svg';
+        plusIcon.alt = '+';
     }
 });
-
 
 const shippingCost = 5.99; // Fiksni trošak dostave i poreza u eurima
 
